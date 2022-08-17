9796a296e47bec29b874da05d4561b33e5e59ab9
EnriqueGmz <enriquegomezs@gmail.com>
2022-08-17 18:15:21 +0200

    Home y Navbar listos

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require("express");
+const { create } = require("express-handlebars");
+
 const app = express();
 
-app.use(express.static(__dirname + "/public"))
-
-app.get("/", (req, res) => {
-    res.send("Estas solicitando la ruta raíz");
+const hbs = create({
+    extname: ".hbs",
+    partialsDir: ["views/components"],
 });
 
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 
+app.get("/", (req, res) => {
+    const urls = [
+        { origin: "www.google.com/EnriqueGmz1", shortURL: "fdaasdasd1" },
+        { origin: "www.google.com/EnriqueGmz2", shortURL: "fdaasdasd2" },
+        { origin: "www.google.com/EnriqueGmz3", shortURL: "fdaasdasd3" },
+        { origin: "www.google.com/EnriqueGmz4", shortURL: "fdaasdasd4" },
+    ];
+    res.render("home", { urls: urls });
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+// Middlewares
+app.use(express.static(__dirname + "/public"))
 
 app.listen(5000, () => console.log("Servidor andando 🚀"));
