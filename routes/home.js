4c5825d019de1cae150d199dbec40f6e429dd278
EnriqueGmz <enriquegomezs@gmail.com>
2022-08-17 20:55:45 +0200

    Programar boton editar Terminado

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { leerUrls, agregarUrl, eliminarUrl } = require("../controllers/homecontroller");
+const { leerUrls, agregarUrl, eliminarUrl, editarUrl, editarUrlForm } = require("../controllers/homecontroller");
+const urlValidar = require("../middlewares/urlValida");
 const router = express.Router();
 
 router.get("/", leerUrls);
-router.post("/", agregarUrl);
+router.post("/", urlValidar, agregarUrl);
 router.get("/eliminar/:id", eliminarUrl);
+router.get("/editar/:id", editarUrlForm);
+router.post("/editar/:id", urlValidar, editarUrl);
 
 module.exports = router;
